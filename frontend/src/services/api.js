a20d20d003f7daa6b9938f26a5a3b24adb83759a
Nanciah <nanciah05@gmail.com>
2025-11-24 01:10:06 +0300

    Fix API URL to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://cisco-majunga-2025.onrender.com/api';
 
 // Configuration axios avec intercepteur pour le token
 const api = axios.create({
